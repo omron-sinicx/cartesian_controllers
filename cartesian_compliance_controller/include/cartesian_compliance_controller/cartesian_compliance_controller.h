623fc9f033859b6524ef56f253fcacedf38f9533
Cristian Beltran <3798796+cambel@users.noreply.github.com>
2025-02-04 11:19:41 +0900

    Refactor code
    - Add option to choose Ioana method or Yamane-san method

--- cartesian_compliance_controller/include/cartesian_compliance_controller/cartesian_compliance_controller.h
+++ cartesian_compliance_controller/include/cartesian_compliance_controller/cartesian_compliance_controller.h
@@ -98,6 +98,15 @@ class CartesianComplianceController
 
     void update(const ros::Time& time, const ros::Duration& period);
 
+    void apply_selection_matrix_gripper_frame(ctrl::Vector6D pos_error_ref,
+                                              ctrl::Vector6D force_error_ref,
+                                              ctrl::Vector6D *pos_error_sel,
+                                              ctrl::Vector6D *force_error_sel,
+                                              ctrl::Matrix6D selection_matrix_gripper,
+                                              ctrl::Matrix3D R_gripper_to_ref);
+
+    void compute_selection_matrix_from_normal_surface(ctrl::Matrix6D *m_selection_matrix_pd);
+
     typedef cartesian_controller_base::CartesianControllerBase<HardwareInterface> Base;
     typedef cartesian_motion_controller::CartesianMotionController<HardwareInterface> MotionBase;
     typedef cartesian_force_controller::CartesianForceController<HardwareInterface> ForceBase;
@@ -113,6 +122,8 @@ class CartesianComplianceController
     ctrl::Matrix6D        m_stiffness;
     ctrl::Matrix6D        m_selection_matrix;
     bool                  m_use_parallel_force_position_control;
+    bool                  m_use_parallel_force_position_control_in_gripper_frame;
+    bool                  m_use_parallel_force_position_control_powder_grinding;
     std::string           m_compliance_ref_link;
 
     // Dynamic reconfigure for stiffness
